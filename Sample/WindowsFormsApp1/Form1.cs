4a73b10e0d26deb95250325d62401309c9393f8d
柳原 <OIBC34063@ojs.ac.jp>
2023-04-17 10:39:36 +0900

    計算処理を分割

--- Sample/WindowsFormsApp1/Form1.cs
+++ Sample/WindowsFormsApp1/Form1.cs
@@ -19,15 +19,18 @@ namespace WindowsFormsApp1 {
             // tbNum1.Text = "楽しいC＃";
             this.BackColor = Color.LightSteelBlue;
             btButton.BackColor = Color.Red;
-            tbAnswer.Text = (int.Parse(tbNum1.Text) + int.Parse(tbNum2.Text)).ToString();
-            
+            //tbAnswer.Text = (int.Parse(tbNum1.Text) + int.Parse(tbNum2.Text)).ToString();
+            int num1 = int.Parse(tbNum1.Text);
+            int num2 = int.Parse(tbNum2.Text);
+            int sum = num1 + num2;
+            tbAnswer.Text = sum.ToString();
+
         }
 
-     
     }
     /* プログラム作成
      * ↓
-     *ビルド Ctrl + Shift + B
+     *ビルド Ctrl + Shift + B　ビルド含め下はCtrl + F5 (F5)
      *↓
      *実行ファイル(.exe)
      *↓ダブルクリック
