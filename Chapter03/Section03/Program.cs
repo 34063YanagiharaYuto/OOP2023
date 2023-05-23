dee89a449ecdde13e7753621facc41690149fea8
柳原 <OIBC34063@ojs.ac.jp>
2023-05-23 11:14:31 +0900

    p90まで

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -10,6 +10,27 @@ namespace Section03 {
             var list = new List<string> {
                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
             };
+
+            //・Existsメソッド
+            //var exists = list.Exists(s => s[0] == 'B');
+            //Console.WriteLine(exists);
+
+            //・Findメソッド 
+            //var name = list.Find(s => s.Length == 6);
+            //Console.WriteLine(name);
+
+            //・FindIndexメソッド
+            //int index = list.FindIndex(s => s == "London");
+            //Console.WriteLine(index);
+
+            //・FindAllメソッド
+            //var names = list.FindAll(s => s.Length <= 5);
+            //names.ForEach(s => Console.WriteLine(s));
+
+            //・ConvertAllメソッド
+            var names = list.ConvertAll(s => s.ToLower());
+            names.ForEach(s => Console.WriteLine(s));
+
         }
     }
 }
