4e708baceae34f30ba74ef35244e71f40cc1fc42
柳原 <OIBC34063@ojs.ac.jp>
2023-05-23 11:54:18 +0900

    第3章の演習問題1

--- Chapter03/Section03/Program.cs
+++ Chapter03/Section03/Program.cs
@@ -8,7 +8,8 @@ namespace Section03 {
     class Program {
         static void Main(string[] args) {
             var list = new List<string> {
-               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+               "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
             };
 
             //・Existsメソッド
