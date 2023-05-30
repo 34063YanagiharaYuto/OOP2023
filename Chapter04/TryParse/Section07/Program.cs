325e07b4a80511903b3f18c493c31f2658439ec4
柳原 <OIBC34063@ojs.ac.jp>
2023-05-30 12:09:32 +0900

    演習問題4.1　YearMonthクラス完成

---
+++ Chapter04/TryParse/Section07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section07 {
+    class Program {
+        static void Main(string[] args) {
+            var str = "123";
+
+            int height;
+            if (int.TryParse(str, out height)) {
+                // heightには変換された値が入っている。
+                Console.WriteLine(height);
+            }
+            else {
+                // 変換に失敗した。
+                Console.WriteLine("変換できません");
+            }
+        }
+
+        // TryParse(変換する文字, out 変換した値を入れる変数)
+    }
+}
---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    // 4.1.1
+    public class YearMonth {
+        public int Year { get; private set; }
+
+        public int Month { get; private set; }
+
+        public YearMonth(int year,int month) {
+            Year = year;
+            Month = month;
+
+        }
+        // 4.1.2
+        public bool Is21Century {
+            get {
+                return 2001 <= Year && Year > 2100;
+            }
+        }
+
+        // 4.1.3
+        public YearMonth AddOneMonth() {
+            if (Month == 12) {
+                return new YearMonth(Year + 1,Month + 1);
+            }
+            else {
+                return new YearMonth(Year, Month + 1);
+            }
+
+            // 一行で書く場合
+            // return new YearMonth(Month == 12 ? +1 : Year, Month == 12 ? 1 : Month + 1);
+        }
+
+        public override string ToString() {
+            return Year + "年" + Month + "月";
+        }
+
+
+    }
+}
