11f7bf5cf7b3982cf559fadc503647505bdba9a9
柳原 <OIBC34063@ojs.ac.jp>
2023-05-31 11:58:00 +0900

    演習問題5.2　完成

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("文字列数字：");
+            var num = Console.ReadLine();
+            int.TryParse(num, out var nums);
+            var number = nums.ToString("#,0");
+            Console.WriteLine(number);
+
+        }
+    }
+}
