930015b0259193b32160895230572a6b59132f63
柳原 <OIBC34063@ojs.ac.jp>
2023-06-05 10:41:06 +0900

    演習問題5.3　完成

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -22,27 +22,40 @@ namespace Exercise03 {
             Console.WriteLine("-----");
 
             Exercise3_5(text);
-            {\rtf1}
+            //{\rtf1}
         }
 
         private static void Exercise3_1(string text) {
-            
+            var list = text.Split(' ').Length - 1;
+            Console.WriteLine("空白数：{0}",list);
         }
 
         private static void Exercise3_2(string text) {
-            
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
         }
 
         private static void Exercise3_3(string text) {
-            
+            string[] words = text.Split(' ');
+            Console.WriteLine("単語数：{0}",words.Length);
         }
 
         private static void Exercise3_4(string text) {
-            
+            var words = text.Split(' ').Where(s => s.Length <= 4);
+            foreach (var word in words) {
+                Console.WriteLine(word);
+                
+            }
         }
 
         private static void Exercise3_5(string text) {
-            
+            var word = text.Split(' ').ToList();
+            var sb = new StringBuilder();
+            foreach(var w in word) {
+                sb.Append(w + ' ');
+            }
+            var texts = sb.ToString();
+            Console.WriteLine(texts);
         }
     }
 }
