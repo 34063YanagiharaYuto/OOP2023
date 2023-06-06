1e93fb418bdb878b8fe138b5026395eb7a230cba
柳原　 悠人 <OIBC34063@ojs.ac.jp>
2023-06-06 11:13:50 +0900

    演習問題5.4　StringArray追加＆一部修正

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,27 +8,50 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
-
+            Stopwatch sw = new Stopwatch();// タイマーインスタンス生成
+            sw.Start();// タイマースタート
 #if NonArray
-
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
 #elif StringArray
-
-            var words = line.Split(';');
-            var startIndex = line.IndexOf("=") + words.Length;
+            
+            string[] lines = {
+            "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+            "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+            "Novelist=太宰治;BestWork=人間失格;Born=1909",
+            "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+            "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+            "Novelist=太宰治;BestWork=人間失格;Born=1909",
+            "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+            "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+            "Novelist=太宰治;BestWork=人間失格;Born=1909",
+            "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+            "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1867",
+            "Novelist=太宰治;BestWork=人間失格;Born=1909",
+            "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896"
+            };
 #endif
+
+#if NonArray
             var words = line.Split(';', '=');
             Console.WriteLine("作家："+  words[1]);
             Console.WriteLine("代表作："+ words[3]);
             Console.WriteLine("誕生年："+ words[5]);
-#if NonArray
-
 
 #elif StringArray
 
+            for (int i = 0; i < lines.Length; i++) {
+                var words = lines[i].Split(';', '=');
+                Console.WriteLine("作家：" + words[1]);
+                Console.WriteLine("代表作：" + words[3]);
+                Console.WriteLine("誕生年：" + words[5]);
+            }
 #endif
-
+            Console.WriteLine("実行時間 = {0}", sw.Elapsed.ToString(@"ss\.fffff")); // 時間表示
 
         }
     }
 }
+
