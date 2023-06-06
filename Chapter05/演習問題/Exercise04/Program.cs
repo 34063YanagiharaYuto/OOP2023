09d15bd8e3c81337b4335c996f746e0fd9c16cca
柳原　 悠人 <OIBC34063@ojs.ac.jp>
2023-06-06 10:09:57 +0900

    演習問題5.4　完成

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -7,7 +7,27 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+
+#if NonArray
+
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+#elif StringArray
+
+            var words = line.Split(';');
+            var startIndex = line.IndexOf("=") + words.Length;
+#endif
+            var words = line.Split(';', '=');
+            Console.WriteLine("作家："+  words[1]);
+            Console.WriteLine("代表作："+ words[3]);
+            Console.WriteLine("誕生年："+ words[5]);
+#if NonArray
+
+
+#elif StringArray
+
+#endif
+
+
         }
     }
 }
