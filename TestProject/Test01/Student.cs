9e90a44578fa399e4a833c5437f2c63720102248
柳原　 悠人 <OIBC34063@ojs.ac.jp>
2023-06-19 15:03:20 +0900

    設問1_Studentクラス完成

--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,6 +1,15 @@
 ﻿namespace Test01 {
     class Student {
+        // コンストラクタ
+        public Student(string name,string subject,int score) {
+            Name = name;
+            Subject = subject;
+            Score = score;
+        }
 
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int Score { get; set; }
 
 
     }
