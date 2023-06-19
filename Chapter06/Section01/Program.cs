6e19288ad334f2e9a41ccbc3cdcdb7dfc6f1bb38
柳原　 悠人 <OIBC34063@ojs.ac.jp>
2023-06-19 13:59:09 +0900

    演習問題6.1　完成

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 9, 7, 5, 4, 2, 5, 4, 0, 4, 1, 0, 4, };
+            Console.WriteLine(numbers.Average());
+
+
+            var books = Books.GetBooks();
+
+            Console.WriteLine(books.Average(b => b.Price)); // Booksの価格の平均を求める
+            Console.WriteLine(books.Max(n => n.Price)); // Booksの最大価格を求める
+
+            // 500円以上の本のタイトルをすべて出力
+            var title = books.Where(t => t.Title.Contains("物語"));
+            var booksobj = books.Where(w => w.Price >= 500).OrderBy(d => d.Price);
+            foreach(var book in booksobj) {
+                Console.WriteLine("本のタイトル：{0}　 価格：{1}円",book.Title,book.Price);
+            }
+            
+
+
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_4(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_5(numbers);
+
+        }
+
+        private static void Exercise1_1(int[] numbers) {
+            var pages = numbers.Max();
+            Console.WriteLine(pages);
+        }
+
+        private static void Exercise1_2(int[] numbers) {
+            var index = numbers.Skip(numbers.Length - 2);
+            foreach (var s in index) {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static void Exercise1_3(int[] numbers) {
+            var select = numbers.Select(s => s.ToString());
+            foreach (var item in select) {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void Exercise1_4(int[] numbers) {
+            var order = numbers.OrderBy(o => o);
+            foreach (var orders in order.Skip(0).Take(3)) {
+                Console.WriteLine(orders);
+            }
+        }
+
+        private static void Exercise1_5(int[] numbers) {
+            var results = numbers.Distinct().Count(c => c > 10);
+            Console.WriteLine(results);
+        }
+    }
+}
