a2809121dc2dca9b362316bb7657b4407cf67dfa
柳原　 悠人 <OIBC34063@ojs.ac.jp>
2023-06-23 11:48:51 +0900

    県庁所在地の登録　完成

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Section01 {
     class Program {
@@ -25,47 +23,57 @@ namespace Section01 {
             //Console.WriteLine("チューリップの価格は{0}円です。", flowerDict["tulip"]);
             //Console.WriteLine("あさがおの価格は{0}円です。", flowerDict["morning glory"]);
             #endregion
-            var Prefectures = new Dictionary<string, string>() {
-            };
-            string Prefecture, PrefecturalCapital;
+
+            #region 県庁所在地の登録プログラム
+            var Prefectures = new Dictionary<string, CityInfo>();
             Console.WriteLine("***県庁所在地の登録***");
             Console.Write("県名:");
-            Prefecture = Console.ReadLine();
-
-            while(Prefecture != "999") {
+            var Prefecture = Console.ReadLine();
+            while (Prefecture != "999") {
+                var cityinfo = new CityInfo();
                 Console.Write("県庁所在地:");
-                PrefecturalCapital = Console.ReadLine();
-                Prefectures[Prefecture] = PrefecturalCapital;
+                cityinfo.City = Console.ReadLine();
+                Prefectures[Prefecture] = cityinfo;
+                Console.Write("人口:");
+                cityinfo.Population = int.Parse(Console.ReadLine());
+                Prefectures[Prefecture] = cityinfo;
 
                 Console.Write("県名:");
                 Prefecture = Console.ReadLine();
-                if (Prefectures.ContainsKey(Prefecture)) {
-                    Console.WriteLine("上書きをしますか？　YES / NO");
-                    var uwagaki = Console.ReadLine();
-                    if(uwagaki == "yes" || uwagaki == "YES") {
-                        Prefectures[Prefecture] = PrefecturalCapital;
-                    } 
-                }
 
+                if (Prefectures.ContainsKey(Prefecture)) {
+                    Console.WriteLine("既に入力されたものがあります。");
+                    Console.WriteLine("上書きをしますか？　yes / no");
+                    var uwagaki = Console.ReadLine();
+                    if (uwagaki == "yes") {
+                        Prefectures[Prefecture] = cityinfo;
+                    }
+                }
             }
 
             Console.WriteLine("1:一覧表示,2:県名指定");
             Console.Write(">");
             var suuji = Console.ReadLine();
-            if(suuji == "1") {
-                foreach(var kennmei in Prefectures) {
-                    Console.WriteLine("{0} ({1})", kennmei.Key, kennmei.Value);
+            if (suuji == "1") {
+                foreach (var kennmei in Prefectures.OrderByDescending(p => p.Value.Population)) {
+        
+                    Console.WriteLine
+                        ("{0} 【{1}(人口：{2}人)】", kennmei.Key, kennmei.Value.City,kennmei.Value.Population);
                 }
-            }else if(suuji == "2") {
+            }
+            else if (suuji == "2") {
                 Console.Write("県名を入力:");
                 var anser = Console.ReadLine();
-                Console.WriteLine("県庁所在地は、{0}です。", Prefectures[anser]);
+                Console.WriteLine("【{0}(人口：{1}人)】", Prefectures[anser].City, Prefectures[anser].Population);
             }
+            #endregion
         }
     }
 
+    #region 県庁所在地の登録のCityInfoクラス
     class CityInfo {
-        public string City { get; set; }
-        public int Population { get; set; }
+        public string City { get; set; }    // 都市
+        public int Population { get; set; } // 人口
     };
+    #endregion
 }
