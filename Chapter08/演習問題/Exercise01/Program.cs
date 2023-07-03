d22bf5153bc93a8e818f6797c7730749d830ff5e
柳原　 悠人 <OIBC34063@ojs.ac.jp>
2023-07-03 13:35:35 +0900

    演習問題8.2　完成

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var dateTime = new DateTime(2019, 1, 15, 19, 48, 32);
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+            DisplayDatePattern3_2(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            // 2019/1/15 19:48
+            var dt1 = dateTime.ToString("g");
+            Console.WriteLine(dt1);
+            // 現在時刻
+            var date = DateTime.Now;
+            var dt2 = date.ToString("g");
+            Console.WriteLine(dt2);
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            // 2019年01月15日 19時48日32秒
+            var dt3 = dateTime.ToString("yyyy年MM月dd日 HH時mm分ss秒");
+            Console.WriteLine(dt3);
+            
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            // 平成31年 1月15日(火曜日)
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var dt4 = dateTime.ToString("ggyy年M月d日(dddd)");
+            Console.WriteLine(dt4);
+        }
+
+        private static void DisplayDatePattern3_2(DateTime dateTime) {
+            
+        }
+    }
+}
---
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var day = DateTime.Now;
+            var syougatuday = new DateTime(2023, 12, 28);
+            var s1 = day.ToString("d");
+            var s2 = syougatuday.ToString("d");
+            foreach (var dayfweek in Enum.GetValues(typeof(DayOfWeek))){
+                Console.Write("{0}の次の週の{1}：",s1,dayfweek);
+                DateTime next = NextDay(day, (DayOfWeek)dayfweek + 7);
+                Console.WriteLine(next.ToString("yyyy/MM/dd(ddd)"));
+            }
+
+            Console.WriteLine(); // 改行
+
+            foreach (var dayfweek in Enum.GetValues(typeof(DayOfWeek))) {
+                Console.Write("{0}の次の週の{1}：", s2, dayfweek);
+                DateTime next = NextDay(syougatuday, (DayOfWeek)dayfweek + 7);
+                Console.WriteLine(next.ToString("yyyy/MM/dd(ddd)"));
+            }
+        }
+
+        public static DateTime NextDay(DateTime date, DayOfWeek dayOfWeek) {
+            var days = (int)dayOfWeek - (int)(date.DayOfWeek);
+            if(days <= 0)
+                days += 7;
+            return date.AddDays(days);
+        }
+    }
+}
