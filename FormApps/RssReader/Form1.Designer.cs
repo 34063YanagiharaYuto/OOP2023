c0689b6b53f97bf9793bd395d52b66e1933a68c4
柳原　悠人 <OIBC34063@ojs.ac.jp>
2023-10-03 11:26:31 +0900

    RssReader クリックの際のウェブサイト表示追加、コンボボックスからのURL取り出し作成途中

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -24,13 +24,85 @@ namespace RssReader {
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.btGet = new System.Windows.Forms.Button();
+            this.lbRssTitle = new System.Windows.Forms.ListBox();
+            this.wbBrowser = new System.Windows.Forms.WebBrowser();
+            this.cbMainRss = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbUrl.Location = new System.Drawing.Point(12, 12);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(821, 31);
+            this.tbUrl.TabIndex = 0;
+            // 
+            // btGet
+            // 
+            this.btGet.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btGet.Location = new System.Drawing.Point(839, 12);
+            this.btGet.Name = "btGet";
+            this.btGet.Size = new System.Drawing.Size(95, 31);
+            this.btGet.TabIndex = 1;
+            this.btGet.Text = "取得";
+            this.btGet.UseVisualStyleBackColor = true;
+            this.btGet.Click += new System.EventHandler(this.btGet_Click);
+            // 
+            // lbRssTitle
+            // 
+            this.lbRssTitle.FormattingEnabled = true;
+            this.lbRssTitle.ItemHeight = 12;
+            this.lbRssTitle.Location = new System.Drawing.Point(12, 61);
+            this.lbRssTitle.Name = "lbRssTitle";
+            this.lbRssTitle.Size = new System.Drawing.Size(922, 172);
+            this.lbRssTitle.TabIndex = 2;
+            this.lbRssTitle.Click += new System.EventHandler(this.lbRssTitle_Click);
+            // 
+            // wbBrowser
+            // 
+            this.wbBrowser.Location = new System.Drawing.Point(12, 251);
+            this.wbBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbBrowser.Name = "wbBrowser";
+            this.wbBrowser.ScriptErrorsSuppressed = true;
+            this.wbBrowser.Size = new System.Drawing.Size(1051, 410);
+            this.wbBrowser.TabIndex = 3;
+            // 
+            // cbMainRss
+            // 
+            this.cbMainRss.FormattingEnabled = true;
+            this.cbMainRss.Location = new System.Drawing.Point(940, 23);
+            this.cbMainRss.Name = "cbMainRss";
+            this.cbMainRss.Size = new System.Drawing.Size(121, 20);
+            this.cbMainRss.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(1075, 673);
+            this.Controls.Add(this.cbMainRss);
+            this.Controls.Add(this.wbBrowser);
+            this.Controls.Add(this.lbRssTitle);
+            this.Controls.Add(this.btGet);
+            this.Controls.Add(this.tbUrl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.Button btGet;
+        private System.Windows.Forms.ListBox lbRssTitle;
+        private System.Windows.Forms.WebBrowser wbBrowser;
+        private System.Windows.Forms.ComboBox cbMainRss;
     }
 }
 
--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -4,14 +4,59 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace RssReader {
     public partial class Form1 : Form {
+        List<ItemData> nodes;
         public Form1() {
             InitializeComponent();
         }
+        
+        private void Form1_Load(object sender, EventArgs e) {
+            using(var wc = new WebClient()) {
+                wc.Encoding = Encoding.UTF8;
+                var url = wc.OpenRead(@"https://news.yahoo.co.jp/rss");
+                XDocument xdoc = XDocument.Load(url);
+                var topics = xdoc.Root.Descendants("li").Descendants("a");
+                foreach(var topic in topics) {
+                    if (topic.FirstAttribute.Value.Contains(".xml")
+                        && !cbMainRss.Items.Contains(topic.Value)){
+                        cbMainRss.Items.Add(topic.Value);
+                    }
+                }
+            }
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbUrl.Text);
+                XDocument xdoc = XDocument.Load(url);
+
+                nodes = xdoc.Root.Descendants("item")
+                                        .Select(x => new ItemData {
+                                            Title = x.Element("title").Value,
+                                            Link = (x.Element("link").Value),
+                                        }).ToList();
+
+                // 一度表に入っているものを削除
+                if (lbRssTitle.Items.Count > 1) {
+                    lbRssTitle.Items.Clear();
+                }
+
+                // 表に追加
+                foreach (var node in nodes) {
+                    lbRssTitle.Items.Add(node.Title);
+                }
+            }
+        }
+
+        private void lbRssTitle_Click(object sender, EventArgs e) {
+            wbBrowser.Url = new Uri(nodes[lbRssTitle.SelectedIndex].Link);
+        } 
     }
 }
