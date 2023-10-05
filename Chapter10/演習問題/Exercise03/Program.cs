ec9dd3fbf8bfceb8ddf594f7e26d30aadfe5ed12
柳原　悠人 <OIBC34063@ojs.ac.jp>
2023-10-05 11:31:04 +0900

    演習問題　Exercise3まで

--- Chapter10/演習問題/Exercise03/Program.cs
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise03 {
@@ -14,9 +15,12 @@ namespace Exercise03 {
                "We reorganized the timetable.",
             };
 
-
-
-
+            foreach(var text in texts) {
+                var matches = Regex.Matches(text,@"\btime\b",RegexOptions.IgnoreCase);
+                foreach(Match m in matches) {
+                    Console.WriteLine("{0}: {1}", text, m.Index);
+                }  
+            }
         }
     }
 }
