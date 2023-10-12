b5a4bd6a586f8db235a93bdbd27af284ec3e34fe
柳原　悠人 <OIBC34063@ojs.ac.jp>
2023-10-12 10:37:18 +0900

    演習問題 15-1-6まで

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -69,7 +69,21 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_6() {
-
+            var groups = Library.Categories
+                                .GroupJoin(Library.Books,
+                                    c => c.Id,
+                                    b => b.CategoryId,
+                                    (c, books) => new {
+                                        Category = c.Name,
+                                        Books = books
+                                    })
+                                .OrderBy(o => o.Category);
+            foreach(var group in groups) {
+                Console.WriteLine("#{0}", group.Category);
+                foreach(var book in group.Books) {
+                    Console.WriteLine($"   {book.Title}");
+                }
+            }
         }
 
         private static void Exercise1_7() {
