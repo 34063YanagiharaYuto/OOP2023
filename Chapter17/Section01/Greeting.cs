32ae99ca30e56f0278ae93cccdf13da308f03ca4
柳原　悠人 <OIBC34063@ojs.ac.jp>
2023-10-17 10:08:45 +0900

    chapter17、Greetingクラス作成

---
+++ Chapter17/Section01/Greeting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class GreetingMorning {
+        public string GetMessage() {
+            return "おはよう";
+        }
+    }
+
+    class GreetingAfternoon {
+        public string GetMessage() {
+            return "こんにちは";
+        }
+    }
+
+    class GreetingEvening {
+        public string GetMessage() {
+            return "こんばんは";
+        }
+    }
+}
