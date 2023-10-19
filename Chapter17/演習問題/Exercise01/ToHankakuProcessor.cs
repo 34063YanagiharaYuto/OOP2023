8e34d3ae73ac0f50d3feac37224322c14b322bda
柳原　悠人 <OIBC34063@ojs.ac.jp>
2023-10-19 10:37:14 +0900

    演習問題17章　Exercise01まで

---
+++ Chapter17/演習問題/Exercise01/ToHankakuProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class ToHankakuProcessor : TextProcessor{
+        private List<string> _items = new List<string>();
+        private static Dictionary<char, char> _dictionary =
+            new Dictionary<char, char>() {
+                {'０', '0'},{'１', '1'},{'２', '2'},{'３', '3'},{'４', '4'},
+                {'５', '5'},{'６', '6'},{'７', '7'},{'８', '8'},{'９', '9'}
+            };
+
+        protected override void Initialize(string fname) {
+            
+        }
+
+        protected override void Execute(string line) {
+            foreach(var d in _dictionary) {
+                line = line.Replace(d.Key, d.Value);
+            }
+            _items.Add(line);
+        }
+
+        protected override void Terminate() {
+            foreach (var item in _items) {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
